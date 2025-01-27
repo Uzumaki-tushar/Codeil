52a7277db7de9e773cded8f225ea197289f337f6
Uzumaki-tushar <tusharsourav01@gamil.com>
2025-01-27 14:56:57 +0530

    First commit: created the directory and setup express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(error){
+    if(error){
+        console.log(`Error in running server : ${error}`);
+        return;
+    }
+
+    console.log(`Server running on port ${port}`);
+})
