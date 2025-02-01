3053f12d41a675222b7237d168fc15ffc02e5761
Uzumaki-tushar <tusharsourav01@gamil.com>
2025-02-02 03:17:37 +0530

    third commit: created user controller and user routes and set up view engine ejs for project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const homeContoller =require('../controllers/home_controllers.js');
 
 router.get('/',homeContoller.home);
-
+router.use('/users',require('./users.js'));
 
 
 module.exports=router;
